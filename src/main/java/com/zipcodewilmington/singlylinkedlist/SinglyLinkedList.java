3709f894763205ffab14e55b81f96680f9adee7c
Keenen <keenen@Zipcoders-MacBook-Pro.local>
2023-08-02 10:30:04 -0400

    added methods stubs

--- src/main/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedList.java
+++ src/main/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedList.java
@@ -1,7 +1,48 @@
 package com.zipcodewilmington.singlylinkedlist;
 
+
+import java.util.Comparator;
+
 /**
  * Created by leon on 1/10/18.
  */
-public class SinglyLinkedList {
+public class SinglyLinkedList<T> implements LinkedListIface {
+
+    public SinglyLinkedList() {}
+
+    public void add(java.lang.Object data) {
+
+    }
+
+    public void remove(int index) throws IndexOutOfBoundsException {
+
+    }
+
+    public java.lang.Object get(int index) throws IndexOutOfBoundsException {
+        return null;
+    }
+
+    public boolean contains(java.lang.Object data) {
+        return false;
+    }
+
+    public int find(java.lang.Object data) {
+        return 0;
+    }
+
+    public int size() {
+        return 0;
+    }
+
+    public SinglyLinkedList copy() {
+        return null;
+    }
+
+    public void sort(Comparator comparator) {
+
+    }
+
+    public void sort(Comparator comparator) {
+
+    }
 }
