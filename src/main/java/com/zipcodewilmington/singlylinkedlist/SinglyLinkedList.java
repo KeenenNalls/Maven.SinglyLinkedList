defdb2866a29b75773a40416727cd76e56d93724
Keenen <keenen@Zipcoders-MacBook-Pro.local>
2023-08-03 08:26:46 -0400

    updates from alumni help

--- src/main/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedList.java
+++ src/main/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedList.java
@@ -2,47 +2,129 @@ package com.zipcodewilmington.singlylinkedlist;
 
 
 import java.util.Comparator;
-
 /**
  * Created by leon on 1/10/18.
  */
-public class SinglyLinkedList<T> implements LinkedListIface {
+public class SinglyLinkedList<AnyType> implements LinkedListIface {
+    private Node<AnyType> head = null;
+    public SinglyLinkedList() {
+    }
 
-    public SinglyLinkedList() {}
-
-    public void add(java.lang.Object data) {
-
+    public void add(Object data) {
+        Node newNode = new Node(data);
+        if (this.head == null){
+            this.head = newNode;
+        } else {
+            Node<AnyType> temp = head;
+            while (temp.hasNext()){
+                temp = temp.getNext();
+            }
+            temp.setNext(newNode);
+        }
     }
 
     public void remove(int index) throws IndexOutOfBoundsException {
+        if (this.size() < 2){
+            head = null;
+        }
+        else if (index == 0){
+            head = head.getNext();
+        } else if (get(index).hasNext()){
+            Node current = get(index);
+            Node previous = get(index - 1);
+            Node after = get (index + 1);
+            previous.setNext(after);
+            current.setNext(null);
+        }
 
     }
 
-    public java.lang.Object get(int index) throws IndexOutOfBoundsException {
-        return null;
+    public boolean contains(Object data) {
+        return this.find(data) != -1;
     }
 
-    public boolean contains(java.lang.Object data) {
-        return false;
-    }
-
-    public int find(java.lang.Object data) {
-        return 0;
+    public int find(Object data) {
+        int result = -1;
+        if (this.isEmpty()){
+            return result;
+        }
+        Node<AnyType> temp = this.head;
+        do{
+            result++;
+            if (temp.getData().equals(data)){
+                return result;
+            }
+            temp = temp.getNext();
+        } while (temp != null);
+        return -1;
     }
 
     public int size() {
-        return 0;
+        if (this.isEmpty()){
+            return 0;
+        }
+        Node<AnyType> temp = this.head;
+        Integer result = 0;
+        do{
+            result++;
+            temp = temp.getNext();
+        } while (temp != null);
+        return result;
     }
 
-    public SinglyLinkedList copy() {
+    public Node get(int index) throws IndexOutOfBoundsException {
+        int result = -1;
+        if (this.isEmpty()){
+            throw new IndexOutOfBoundsException();
+        }
+        Node temp = this.head;
+        do{
+            result++;
+            if (result == index){
+                return temp;
+            }
+            temp = temp.getNext();
+        } while (temp != null);
         return null;
     }
 
-    public void sort(Comparator comparator) {
 
+    public SinglyLinkedList copy() {
+        //return null;
+
+        SinglyLinkedList result = new SinglyLinkedList();
+        if(this.isEmpty()){
+            return result;
+        }
+        Node temp = this.head;
+        while(temp.hasNext()){
+            result.add(temp.getData());
+            temp = temp.getNext();
+        }
+        result.add(temp.getData());
+        return result;
     }
 
-    public void sort(Comparator comparator) {
 
+    public void sort(Comparator comparator) {
+    }
+    public Boolean isEmpty(){
+        return head == null;
+    }
+    @Override
+    public String toString() {
+        StringBuilder string = new StringBuilder();
+        string.append("head -> ");
+        Node<AnyType> temp = this.head;
+        if (!this.isEmpty()){
+            do{
+                string.append("[")
+                        .append(temp.getData())
+                        .append("] -> ");
+                temp = temp.getNext();
+            } while (temp != null);
+        }
+        string.append("NULL");
+        return string.toString();
     }
 }
--- src/test/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedListTest.java
+++ src/test/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedListTest.java
@@ -1,7 +1,24 @@
 package com.zipcodewilmington.singlylinkedlist;
+import org.junit.Assert;
+import org.junit.Test;
+
+
+
 
 /**
  * Created by leon on 1/10/18.
  */
+
 public class SinglyLinkedListTest {
+    @Test
+    public void copyTest1() {
+        SinglyLinkedList<Integer> expected = new SinglyLinkedList<>();
+        expected.add(1);
+        expected.add(2);
+        expected.add(5);
+
+        SinglyLinkedList<Integer> actual = expected.copy();
+
+        Assert.assertEquals(expected.toString(), actual.toString());
+    }
 }
